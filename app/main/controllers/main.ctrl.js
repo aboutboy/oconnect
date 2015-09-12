67eb6cf42bd863abb7c479ed13de85a5fed55754
Sabbir Ahmed <sabbiirr@gmail.com>
2015-09-12 20:29:11 +0600

    Fix: icon from localresource, Added: swipe for sidebar

--- app/main/controllers/main.ctrl.js
+++ app/main/controllers/main.ctrl.js
@@ -9,4 +9,12 @@ angular.module('main').controller('MainCtrl', function ($scope, $mdSidenav, stor
   if (!store.get('host')) {
     store.set('host', '192.168.1.1');
   }
+
+  $scope.showSidenav = function (menuId) {
+    $mdSidenav(menuId).open();
+  };
+  $scope.hideSidenav = function (menuId) {
+    $mdSidenav(menuId).close();
+  };
+
 });
